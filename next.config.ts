bbf328b590783171e0c869f9e2e0f5b5b260c55d
HimeshPremji <himeshdmaheswary@gmail.com>
2025-03-25 23:51:13 +0500

    time for initial deployment and tests 13

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-//     custom image domains here
-    experimental: {
-        useDeploymentId: true,
-        // Optionally, use with Server Actions
-        useDeploymentIdServerActions: true,
-    },
     images: {
         remotePatterns: [
-            // {
-            //     protocol: 'https',
-            //     hostname: 'picsum.photos',
-            //     pathname: '/**',
-            // },
             {
                 protocol: 'https',
                 hostname: '**',
