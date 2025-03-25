0b68a457c1af7f0b598225f92b058ea323093000
HimeshPremji <himeshdmaheswary@gmail.com>
2025-03-26 00:06:58 +0500

    refactor: clean up formatting and add Vercel Analytics dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,30 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
-import {Work_Sans} from 'next/font/google';
+import { Work_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 
 const workSans = Work_Sans({
-    subsets: ['latin-ext'],
-    variable: '--font-sans',
+  subsets: ["latin-ext"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-    title: "Hems Directory",
-    description: "Pitch, Vote and Grow",
+  title: "Hems Directory",
+  description: "Pitch, Vote and Grow",
 };
 
-export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <html lang="en">
-        <body className={workSans.className}> {/* Use the font's className */}
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={workSans.className}>
+        {" "}
+        {/* Use the font's className */}
         {children}
-        </body>
-        </html>
-    );
+        <Analytics />
+      </body>
+    </html>
+  );
 }
